e4451f8e1459696f01cb000541b60f1e82157ee6
Alexander Dodson <alexander.dodson@answerrocket.com>
2022-08-18 11:01:52 -0400

    add campaign stats, point source dep at corresponding test branch

--- models/url_google_ads/google_ads__url_ad_adapter.sql
+++ models/url_google_ads/google_ads__url_ad_adapter.sql
@@ -16,7 +16,13 @@ with stats as (
     select *
     from {{ var('campaign_history') }}
     where is_most_recent_record = True
-    
+), campaign_stats as (
+
+    -- ad_stats lacks performance max since those are campaign level only
+    select *
+    from {{ var('campaign_stats') }}
+    where advertising_channel_type = "PERFORMANCE_MAX"
+
 ), ad_groups as (
 
     select *
@@ -67,6 +73,36 @@ with stats as (
     left join accounts
         on campaigns.account_id = accounts.account_id
     {{ dbt_utils.group_by(15) }}
+
+    union
+
+    select
+        campaign_stats.date_day,
+        accounts.account_name as account_name,
+        campaign_stats.account_id,
+        campaigns.campaign_name,
+        campaign_stats.campaign_id,
+        null as ad_group_name,
+        null as ad_group_id,
+        null as base_url,
+        null as url_host,
+        null as url_path,
+        null as utm_source,
+        null as utm_medium,
+        null as utm_campaign,
+        null as utm_content,
+        null as utm_term,
+        sum(campaign_stats.spend) as spend,
+        sum(campaign_stats.clicks) as clicks,
+        sum(campaign_stats.impressions) as impressions,
+        sum(campaign_stats.conversions) as conversions,
+        sum(campaign_stats.conversions_value) as conversions_value
+    from campaign_stats
+    join campaigns on campaign_stats.campaign_id = campaigns.campaign_id
+    left join accounts
+        on campaign_stats.account_id = accounts.account_id
+    {{ dbt_utils.group_by(15) }}
+
 
 )
 
